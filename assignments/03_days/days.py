eaba1d0debba646adb32de9d4a22f4461a12e18e
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2020-02-18 13:47:14 -0700

    It works!

--- assignments/03_days/days.py
+++ assignments/03_days/days.py
@@ -27,7 +27,7 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    """Check list for days and return string of what to do each day"""
+    """Check list for day/s and return string of what to do each day"""
 
     args = get_args()
     days = args.days
