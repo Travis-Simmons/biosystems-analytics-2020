d48def39aa825c3a587cc0e5098e9b396dff7495
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2020-03-03 00:11:41 -0700

    It works!

---
+++ assignments/05_hamming/hamming.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Author : Emmanuel Gonzalez
+Date   : 2020-02-28
+Purpose: Calcualte the Hamming distance between two string of equal of different lengths.
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Hamming Distance',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        help='Input file')
+
+    parser.add_argument('-m',
+                        '--min',
+                        help='Minimum integer value of the Hamming distance',
+                        metavar='int',
+                        type=int,
+                        default=0)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    with open(args.file) as f:
+        for line in f:
+            line = line.strip()
+            words = line.split()
+            word1 = words[0]
+            word2 = words[1]
+
+            if len(word1) == len(word2):
+                diffs = 0
+                for ch1, ch2 in zip(word1, word2):
+                    if ch1 != ch2:
+                        diffs += 1
+                diffs
+            else:
+                len_diff = abs(len(word1) - len(word2))
+                diffs = 0
+                for ch1, ch2 in zip(word1, word2):
+                    if ch1 != ch2:
+                        diffs += 1
+                diffs += len_diff
+
+            if args.min != 0:
+                if diffs >= args.min:
+                    print(f'{diffs:8}:{word1:20}{word2:20}')
+                else:
+                    None
+            else:
+                print(f'{diffs:8}:{word1:20}{word2:20}')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
