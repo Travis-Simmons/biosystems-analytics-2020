b16cd60a47670703088d2b746799fecb9eda05e2
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2020-03-24 21:32:08 -0700

    It works!

--- assignments/07_rna/transcribe.py
+++ assignments/07_rna/transcribe.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
-Author : emmanuelgonzalez
+Author : Emmanuel Gonzalez
 Date   : 2020-03-19
-Purpose: Rock the Casbah
+Purpose: Transcribe DNA to RNA
 """
 
 import argparse
 import os
-import sys
 
 
 # --------------------------------------------------
@@ -36,48 +35,46 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Transcribe here"""
 
     args = get_args()
-     
     if not os.path.isdir(args.outdir):
         os.makedirs(args.outdir)
     f_cnt = 0
-    for f in args.file: 
+    s_cnt = 0
+    for f in args.file:
         f_cnt += 1
         name = f.name
         out_file = os.path.join(args.outdir, os.path.basename(name))
-        #print(out_file)
         out_fh = open(out_file, 'wt')
         replace = 'T'
-        s_cnt = 0
         for row in f:
-            s_cnt += 1
-            seq = str(row.split()[0])
-            #print("Seq:", seq)
+            seqs = row.split()[:]
             trans = []
-            for ch in seq:
-                #print(ch)
-                #trans = []
-                if ch in replace:
-                    trans.append('U')
-                else:
-                    trans.append(ch)
-            trans_seq = ''.join(trans)
-            #print(trans_seq)
-            #print("trans:", trans) 
-        out_fh.write(trans_seq)
-    #print(f_cnt, s_cnt)
-    plural = {'plural': 'sequences files', 'single': 'sequence file'}
-    print(plural)
+            for seq in seqs:
+                s_cnt += 1
+                for ch in seq:
+                    if ch in replace:
+                        trans.append('U')
+                    else:
+                        trans.append(ch)
+                trans_seq = ''.join(trans)
+            out_fh.write(trans_seq + '\n')
 
-    #if s_cnt and f_cnt == 1:
-    #    print(f'Done, wrote {s_cnt} sequence in {f_cnt} file to directory "{args.outdir}".') 
-    #if s_cnt and f_cnt > 1 :
-    #    print(f'Done, wrote {s_cnt} sequences in {f_cnt} files to directory "{args.outdir}".')
+    if s_cnt == 1 and f_cnt > 1:
+        print(
+            f'Done, wrote {s_cnt} sequence in {f_cnt} files to directory "{args.outdir}".')
+    elif s_cnt > 1 and f_cnt == 1:
+        print(
+            f'Done, wrote {s_cnt} sequences in {f_cnt} file to directory "{args.outdir}".')
+    elif s_cnt > 1 and f_cnt > 1:
+        print(
+            f'Done, wrote {s_cnt} sequences in {f_cnt} files to directory "{args.outdir}".')
+    else:
+        print(
+            f'Done, wrote {s_cnt} sequence in {f_cnt} file to directory "{args.outdir}".')
 
-        #if f_cnt > 1:
-        #    print(f'Done, wrote {s_cnt} sequences in {f_cnt} files to directory "{args.outdir}".')
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
