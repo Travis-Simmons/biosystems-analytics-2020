fbc238ddd0f8cc10f7cfde204f0f7f92b159ed04
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2020-04-29 20:52:27 -0700

    It works!

--- assignments/11_swisstake/swisstake.py
+++ assignments/11_swisstake/swisstake.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : emmanuelgonzalez
+Author : Emmanuel Gonzalez
 Date   : 2020-04-29
-Purpose: Rock the Casbah
+Purpose: Parse SwissProt records
 """
 
 import argparse
